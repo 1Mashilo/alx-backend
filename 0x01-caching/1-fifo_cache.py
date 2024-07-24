c8584fb5f334f5837cedd3531da9c4a8d3f96a5e
1Mashilo <christophermashilo@gmail.com>
2024-07-24 12:43:06 +0200

    improved documentation

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,6 +4,7 @@ This module defines the FIFOCache class, which inherits from BaseCaching and
 implements a FIFO caching system.
 """
 
+
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
