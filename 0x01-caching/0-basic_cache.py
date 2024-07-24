59115e42833ae1ee173ce33468c0a93022243750
1Mashilo <christophermashilo@gmail.com>
2024-07-24 11:57:13 +0200

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
